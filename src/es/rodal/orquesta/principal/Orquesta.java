ebcb4c622caea808537f8cb47addf43904d73210
raulrodals <raul.rodals@viewnext.com>
2024-04-12 10:29:55 +0200

    añadido List = ArrayList

--- src/es/rodal/orquesta/principal/Orquesta.java
+++ src/es/rodal/orquesta/principal/Orquesta.java
@@ -1,5 +1,8 @@
 package es.rodal.orquesta.principal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import es.rodal.orquesta.clases.Flauta;
 import es.rodal.orquesta.clases.Guitarra;
 import es.rodal.orquesta.clases.GuitarraElectrica;
@@ -17,16 +20,16 @@ public class Orquesta {
 	public static void main(String[] args) {
 
 		//Creación de cada uno de los objetos almacenados en un array de Instrumentos
-		Instrumento[] instrumentos = {
-				new Flauta(),
-				new Flauta("flauta", TipoInstrumento.Viento, "travesera"),
-				new Guitarra(),
-				new Guitarra("guitarra", TipoInstrumento.Cuerda, 6),
-				new GuitarraElectrica(),
-				new GuitarraElectrica("guitarra electrica", TipoInstrumento.Cuerda, 6, 20),
-				new Tambor(),
-				new Tambor("tambor", TipoInstrumento.Percusion, 2)
-		};
+		List<Instrumento> instrumentos = new ArrayList<>();
+		instrumentos.add(new Flauta());
+		instrumentos.add(new Flauta("flauta", TipoInstrumento.Viento, "travesera"));
+		instrumentos.add(new Guitarra());
+		instrumentos.add(new Guitarra("guitarra", TipoInstrumento.Cuerda, 6));
+		instrumentos.add(new GuitarraElectrica());
+		instrumentos.add(new GuitarraElectrica("guitarra electrica", TipoInstrumento.Cuerda, 6, 20));
+		instrumentos.add(new Tambor());
+		instrumentos.add(new Tambor("tambor", TipoInstrumento.Percusion, 2));
+		
 		
 		//Bucle en el que llamo al metodo tocar() a menos que el instrumento sea un Tambor
 		for (Instrumento instrumento : instrumentos) {
